68eb03e2ce38a4cdfb9fde383c5cd00797612b64
Arda Aras <ardapatu@gmail.com>
2025-11-28 12:24:27 +0300

    Kaynak kod

---
+++ Kaç_adımda_0_a_ulaşır.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+
+	int sayi, sayac;
+	sayac = 0;
+
+	cout << "Kac adimda 0'a ulasacaginin hesaplanmasi icin bir sayi giriniz:";
+	cin >> sayi;
+
+	while (sayi > 1) {
+
+		if (sayi % 2 == 0) {
+
+			sayi /= 2;
+
+			sayac++;
+
+			cout << sayi << endl;
+		}
+		
+		else {
+
+			sayi -= 1;
+
+			sayac++;
+
+			cout << sayi << endl;
+		}
+	}
+    cout << "0" << endl;
+	
+	cout << sayac << " adimda 0'a ulasir";
+
+	return 0;
+}
